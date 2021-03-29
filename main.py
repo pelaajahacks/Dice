737ed006b89218c0ec2ce4cca6d72df331b4b620
pelaajahacks <ronttipontti.sundstrom2@gmail.com>
2021-03-29 22:21:59 +0300

    Intial Commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import random
+
+import pygame
+
+pygame.init()
+
+number = 0
+rolling = 0
+rollspeed = 1000
+
+screen = pygame.display.set_mode((800, 600))
+clock = pygame.time.Clock
+
+running = True
+while running:
+    screen.fill((255, 255, 255))
+
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                last_number = number
+                while True:
+                    number = random.randint(1, 6)
+                    if last_number == number:
+                        print("frick")
+                    else:
+                        break
+
+        if event.type == pygame.QUIT:
+            running = False
+
+    font = pygame.font.SysFont(None, 100)
+    img = font.render(str(number), True, (0, 0, 0))
+
+    screen.blit(img, (400, 300))
+    pygame.display.update()
